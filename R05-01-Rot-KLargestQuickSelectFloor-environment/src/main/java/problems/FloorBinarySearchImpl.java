8f8ed4920ff83ddaa6a896b00a8a851e1474e7e0
wendelltmo <wendelltmo2306@outlook.com>
2023-09-02 23:29:27 -0300

    modified:   R04-01-Rot-LinearSorting-Countingsort-environment/122110748-send.log 	modified:   R04-01-Rot-LinearSorting-Countingsort-environment/target/122110748.zip 	modified:   R04-01-Rot-LinearSorting-Countingsort-environment/target/surefire-reports/TEST-sorting.test.StudentSortingTest.xml 	modified:   R04-01-Rot-LinearSorting-Countingsort-environment/target/surefire-reports/sorting.test.StudentSortingTest.txt 	modified:   R05-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java 	modified:   R05-01-Rot-KLargestQuickSelectFloor-environment/target/classes/orderStatistic/KLargest.class 	modified:   R05-01-Rot-KLargestQuickSelectFloor-environment/target/classes/orderStatistic/KLargestOrderStatisticsImpl.class 	modified:   R05-01-Rot-KLargestQuickSelectFloor-environment/target/classes/orderStatistic/QuickSelect.class 	modified:   R05-01-Rot-KLargestQuickSelectFloor-environment/target/classes/problems/FloorBinarySearchImpl.class 	modified:   R05-01-Rot-KLargestQuickSelectFloor-environment/target/classes/util/Util.class

--- R05-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
+++ R05-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
@@ -4,8 +4,40 @@ public class FloorBinarySearchImpl implements Floor {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		// TODO implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		int res = -1;
+		
+		if (array != null && array.length != 0) {
+			int left = 0;
+			int right = array.length - 1;
+			res = floorBinarySearch(array, left, right, x);
+		}
+
+		return res;
 	}
+
+	private int floorBinarySearch(Integer[] array, int left, int right, int target) {
+		int res = -1;
+
+		if (left > -1 && left <= right && right < array.length) {
+			int mid = (left + right) / 2;
+			
+			if (array[mid] == target ) {
+				res = array[mid];
+			} else if (array[mid] < target) {
+				int temp_res = array[mid];
+				res = floorBinarySearch(array, mid + 1, right, target); 
+				if (res == -1) {
+					res = temp_res;
+				}
+
+			} else {
+				res = floorBinarySearch(array, left, mid - 1, target);
+			}
+		}
+
+		return res;
+	}
+
 
 }
