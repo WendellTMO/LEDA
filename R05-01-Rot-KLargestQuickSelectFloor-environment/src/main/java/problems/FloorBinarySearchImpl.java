0ffe4823ab4e510f71ac67f3efd5c38beebcf403
wendelltmo <wendelltmo2306@outlook.com>
2023-09-02 23:32:41 -0300

    modified:   R05-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java 	modified:   R05-01-Rot-KLargestQuickSelectFloor-environment/target/classes/problems/FloorBinarySearchImpl.class

--- R05-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
+++ R05-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
@@ -5,27 +5,27 @@ public class FloorBinarySearchImpl implements Floor {
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
 
-		int res = -1;
+		Integer res = -1;
 		
 		if (array != null && array.length != 0) {
-			int left = 0;
-			int right = array.length - 1;
+			Integer left = 0;
+			Integer right = array.length - 1;
 			res = floorBinarySearch(array, left, right, x);
 		}
 
 		return res;
 	}
 
-	private int floorBinarySearch(Integer[] array, int left, int right, int target) {
-		int res = -1;
+	private Integer floorBinarySearch(Integer[] array, Integer left, Integer right, Integer target) {
+		Integer res = -1;
 
 		if (left > -1 && left <= right && right < array.length) {
-			int mid = (left + right) / 2;
+			Integer mid = (left + right) / 2;
 			
 			if (array[mid] == target ) {
 				res = array[mid];
 			} else if (array[mid] < target) {
-				int temp_res = array[mid];
+				Integer temp_res = array[mid];
 				res = floorBinarySearch(array, mid + 1, right, target); 
 				if (res == -1) {
 					res = temp_res;
@@ -38,6 +38,5 @@ public class FloorBinarySearchImpl implements Floor {
 
 		return res;
 	}
-
 
 }
