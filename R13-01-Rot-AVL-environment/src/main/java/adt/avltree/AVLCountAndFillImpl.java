cff4167ef7c3f8a80402c7e76b2b64f1cc94e72e
wendelltmo <wendelltmo2306@outlook.com>
2023-10-26 00:20:19 -0300

    avl update 	modified:   R13-01-Rot-AVL-environment/122110748-send.log 	modified:   R13-01-Rot-AVL-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java 	modified:   R13-01-Rot-AVL-environment/target/122110748.zip 	modified:   R13-01-Rot-AVL-environment/target/classes/adt/avltree/AVLCountAndFillImpl.class 	modified:   R13-01-Rot-AVL-environment/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst 	modified:   R13-01-Rot-AVL-environment/target/submission-1.0.jar 	modified:   R13-01-Rot-AVL-environment/target/surefire-reports/TEST-adt.avltree.StudentAVLTest.xml 	new file:   R13-01-Rot-AVL-environment/target/surefire-reports/TEST-adt.bst.BSTImplTest.xml 	new file:   R13-01-Rot-AVL-environment/target/surefire-reports/TEST-adt.bst.BSTVerifierImplTest.xml 	modified:   R13-01-Rot-AVL-environment/target/surefire-reports/adt.avltree.StudentAVLTest.txt 	new file:   R13-01-Rot-AVL-environment/target/surefire-reports/adt.bst.BSTImplTest.txt 	new file:   R13-01-Rot-AVL-environment/target/surefire-reports/adt.bst.BSTVerifierImplTest.txt 	modified:   R13-01-Rot-AVL-environment/target/test-classes/adt/avltree/StudentAVLTest.class 	modified:   R13-01-Rot-AVL-environment/target/test-classes/adt/avltree/StudentTestAVLCountAndFill.class 	modified:   R13-01-Rot-AVL-environment/target/test-classes/adt/bst/BSTImplTest.class 	modified:   R13-01-Rot-AVL-environment/target/test-classes/adt/bst/BSTVerifierImplTest.class

--- R13-01-Rot-AVL-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java
+++ R13-01-Rot-AVL-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java
@@ -1,5 +1,9 @@
 package adt.avltree;
 
+import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
+import adt.bt.Util;
+
 public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 		AVLTreeImpl<T> implements AVLCountAndFill<T> {
 
@@ -34,8 +38,51 @@ public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 
 	@Override
 	public void fillWithoutRebalance(T[] array) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		BSTImpl<T> withoutRebalance = new BSTImpl<>();
+		for (T i : array) {
+			withoutRebalance.insert(i);
+		}
+
+		root = withoutRebalance.getRoot();
+	}
+
+	@Override	
+	protected void rebalance(BSTNode<T> node) {
+		int balance = calculateBalance(node);
+		if (Math.abs(balance) > 1) {
+			int rightBalance = calculateBalance((BSTNode<T>) node.getRight());
+			int leftBalance = calculateBalance((BSTNode<T>) node.getLeft());
+
+			BSTNode<T> newNode = null;
+			
+			if (balance == 2) {
+				// LL case 
+				if (leftBalance >= 0) {
+					newNode = Util.rightRotation(node);
+					LLcounter++;
+				} else {
+					// LR case
+					Util.leftRotation((BSTNode<T>) node.getLeft());
+					newNode = Util.rightRotation(node);
+					LRcounter++;
+				}
+
+			} else {
+				// RR Case
+				if (rightBalance <= 0) {
+					newNode = Util.leftRotation(node);
+					RRcounter++;
+				} else {
+					// RL case
+					Util.rightRotation((BSTNode<T>) node.getRight());
+					newNode = Util.leftRotation(node);
+					RLcounter++;
+				}
+			}
+			if (getRoot().equals(node) && newNode != null) {
+				root = newNode;
+			}
+		}
 	}
 
 }
